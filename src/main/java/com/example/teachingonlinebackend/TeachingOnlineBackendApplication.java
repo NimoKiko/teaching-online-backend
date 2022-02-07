a57775ca7d531bd05a4da0648298475739ffcb9b
NikoKiko <973528232@qq.com>
2022-02-07 20:22:44 +0800

    teaching-online-backend

---
+++ src/main/java/com/example/teachingonlinebackend/TeachingOnlineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.teachingonlinebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachingOnlineBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeachingOnlineBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teachingonlinebackend/TeachingOnlineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teachingonlinebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachingOnlineBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
